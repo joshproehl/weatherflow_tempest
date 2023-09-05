50f131707fdc03677035868220d4c0f313ecf36a
Josh Proehl <josh@daedalusdreams.com>
2023-09-05 12:33:49 -0500

    Delegate docs for WeatherflowTempest, now that we have new ExDoc

--- lib/weatherflow_tempest.ex
+++ lib/weatherflow_tempest.ex
@@ -1,15 +1,10 @@
 defmodule WeatherflowTempest do
-  # doc delegation won't work before we're on ExDoc 0.23
-  
-  #@doc delegate_to: {WeatheflowTempest.Client, :get_latest, 0}
-  @doc "Delegated to `WeatherflowTempest.Client.get_latest/0` for convenience"
+  @doc delegate_to: {WeatheflowTempest.Client, :get_latest, 0}
   defdelegate get_latest, to: WeatherflowTempest.Client
 
-  #@doc delegate_to: {WeatheflowTempest.Client, :get_packet_stats, 0}
-  @doc "Delegated to `WeatherflowTempest.Client.get_packet_stats/0` for convenience"
+  @doc delegate_to: {WeatheflowTempest.Client, :get_packet_stats, 0}
   defdelegate get_packet_stats, to: WeatherflowTempest.Client
 
-  #@doc delegate_to: {WeatheflowTempest.Client, :get_hub_serials, 0}
-  @doc "Delegated to `WeatherflowTempest.Client.get_hub_serials/0` for convenience"
+  @doc delegate_to: {WeatheflowTempest.Client, :get_hub_serials, 0}
   defdelegate get_hub_serials, to: WeatherflowTempest.Client
 end
