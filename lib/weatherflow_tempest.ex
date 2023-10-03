e8300f703e97e7744b6e756eb277f13c07f58e9e
Josh Proehl <josh@daedalusdreams.com>
2023-10-03 12:59:14 -0500

    Add subscribe_to_udp_events as a delegated method to WeatherflowTempest

--- lib/weatherflow_tempest.ex
+++ lib/weatherflow_tempest.ex
@@ -1,4 +1,9 @@
 defmodule WeatherflowTempest do
+  @moduledoc """
+  This module is merely used as a convenience for accessing the non-OTP
+  functions of the overall library.
+  """
+
   @doc delegate_to: {WeatheflowTempest.Client, :get_latest, 0}
   defdelegate get_latest, to: WeatherflowTempest.Client
 
@@ -7,4 +12,7 @@ defmodule WeatherflowTempest do
 
   @doc delegate_to: {WeatheflowTempest.Client, :get_hub_serials, 0}
   defdelegate get_hub_serials, to: WeatherflowTempest.Client
+
+  @doc delegate_to: {WeatheflowTempest.PubSub, :subscribe_to_udp_events, 0}
+  defdelegate subscribe_to_udp_events, to: WeatherflowTempest.PubSub
 end
